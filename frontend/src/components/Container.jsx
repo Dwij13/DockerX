bfcffda0a40503e1e36cc2c63dd2466e4d9cfec0
Dwij13 <dwijjindal2005@gmail.com>
2024-03-29 11:10:48 +0530

    addded data

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,63 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import "./Container.css";
 import { useParams } from "react-router";
 import axios from "axios";
 
 export default function Container() {
   const { id } = useParams();
   const [containerData, setContainerData] = useState({});
-  const [cpuUsage, setCpuUsage] = useState([]);
+  const [cpuUsage, setCpuUsage] = useState({});
+  const [memoryUsage, setMemoryUsage] = useState({});
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:6969/containers/${id}`)
-      .then((res) => {
-        setContainerData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const fetchData = async () => {
+      try {
+        const containerRes = await axios.get(
+          `http://localhost:6969/containers/${id}`
+        );
+        setContainerData(containerRes.data);
+
+        const statusRes = await axios.get(
+          `http://localhost:6969/containers/${id}/status`
+        );
+        setCpuUsage(statusRes.data.cpu_stats);
+        setMemoryUsage(statusRes.data.memory_stats);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
   }, [id]);
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:6969/containers/${id}/status`)
-      .then((res) => {
-        const cpuStats = res.data.cpu_stats;
-        const formattedData = cpuStats.map((stat, index) => ({
-          name: index,
-          usage: stat.total_usage,
-        }));
-        console.log("Formatted CPU Usage Data:", formattedData); // Log formatted data
-        setCpuUsage(formattedData);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [id]);
-
-  console.log("CPU Usage Data:", cpuUsage); // Log CPU usage data
-
+  console.log(containerData);
+  console.log(cpuUsage);
+  console.log(memoryUsage);
   return (
     <div>
-      <div>
-        <h2>Container Information</h2>
+      <div className="top-div">
+        <h2 className="head">Container Information</h2>
         <div className="list_group">
           <div className="list_inner">
             <div className="list_item">Name: {containerData.Name}</div>
+            <div className="list_item">
+              Image: {containerData.Config?.Image}
+            </div>
+            <div className="list_item">Path: {containerData.Path}</div>
+            <div className="list_item">
+              Status: {containerData.State?.Status}
+              {/* <div
+                className={
+                  containerData.State.Status == "running" ? "green_circle" : "red_circle"
+                }
+              ></div> */}
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <h2>CPU Usage Graph</h2>
-        <LineChart width={800} height={400} data={cpuUsage}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="usage" stroke="#8884d8" />
-        </LineChart>
+      <div className="mid-div">
+        <h2 className="head">CPU Usage Graph</h2>
       </div>
     </div>
   );
